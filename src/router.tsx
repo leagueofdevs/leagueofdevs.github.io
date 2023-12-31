e8afbd3cd02e3bd0ae7243f68160804047761a6d
Mateus Elias de Andrade Pereira <meap@cin.ufpe.br>
2023-12-31 10:30:59 -0300

    feat(src/router.tsx): create route to Teams page

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Teams from "./pages/Teams";
 
-const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/equipes", element: <Teams /> },
+]);
 
 export default router;
