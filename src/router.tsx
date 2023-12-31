3ed26a016f56dffa2f4ea670b51e8aa44d4e3e9a
Mateus Elias de Andrade Pereira <meap@cin.ufpe.br>
2023-12-31 14:30:08 -0300

    feat(src/router.tsx): create Router to results page

--- src/router.tsx
+++ src/router.tsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Teams from "./pages/Teams";
+import Results from "./pages/Results";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/equipes", element: <Teams /> },
+  { path: "/resultados", element: <Results /> },
 ]);
 
 export default router;
