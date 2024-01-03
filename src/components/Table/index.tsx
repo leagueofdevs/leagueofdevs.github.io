6c1c3f1a4af7323bd16cf76f0cc01cffbaae282b
Mateus Elias <meap@cin.ufpe.br>
2024-01-03 06:00:11 -0300

    feat(src/components/Table): add header title

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -36,97 +36,102 @@ function Table({ items }: TableProps) {
   }, [items]);
 
   return (
-    <div className="bg-gray-800 p-4 rounded-lg overflow-x-auto">
-      <table className="w-full whitespace-nowrap text-center">
-        <colgroup>
-          <col className="w-1/20" />
-          <col className="w-3/20" />
-          <col className="w-3/20" />
-          <col className="w-3/20" />
-          <col className="w-3/20" />
-        </colgroup>
-        <thead className="border-b border-white/10 text-lg leading-6 text-white">
-          <tr>
-            <th
-              scope="col"
-              className="py-2 pl-0 pr-4 font-bold text-center sm:pr-8 sm:text-center"
-            >
-              #
-            </th>
-            <th
-              scope="col"
-              className="py-2 pl-4 pr-8 font-bold sm:pl-6 lg:pl-8"
-            >
-              Equipe
-            </th>
-            <th
-              scope="col"
-              className="py-2 pl-0 pr-4 font-bold text-center sm:pr-8 sm:text-center"
-            >
-              <span className="hidden sm:inline">Jogos</span>
-              <span className="sm:hidden">J</span>
-            </th>
-            <th
-              scope="col"
-              className="py-2 pl-0 pr-4 font-bold text-center sm:pr-8 sm:text-center"
-            >
-              <span className="hidden sm:inline">Vitórias</span>
-              <span className="sm:hidden">V</span>
-            </th>
-            <th
-              scope="col"
-              className="py-2 pl-0 pr-4 font-bold text-center sm:pr-8 sm:text-center"
-            >
-              <span className="hidden sm:inline">Derrotas</span>
-              <span className="sm:hidden">D</span>
-            </th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-white/5">
-          {sortedItems &&
-            sortedItems.map((item) => (
-              <tr
-                key={item.acronym}
-                className="transition-all hover:bg-gray-700 cursor-pointer transform hover:scale-95"
+    <div className="flex flex-col gap-y-6">
+      <h4 className="flex text-xl font-bold tracking-tight text-white sm:text-2xl justify-start">
+        Tabela de resultados
+      </h4>
+      <div className="bg-gray-800 p-4 rounded-lg overflow-x-auto">
+        <table className="w-full whitespace-nowrap text-center">
+          <colgroup>
+            <col className="w-1/20" />
+            <col className="w-3/20" />
+            <col className="w-3/20" />
+            <col className="w-3/20" />
+            <col className="w-3/20" />
+          </colgroup>
+          <thead className="border-b border-white/10 text-lg leading-6 text-white">
+            <tr>
+              <th
+                scope="col"
+                className="py-2 pl-0 pr-4 font-bold text-center sm:pr-8 sm:text-center"
               >
-                <td className="py-3 pl-0 pr-1 text-center sm:pr-8 sm:text-center">
-                  {item.position}
-                </td>
-                <td className="py-3 pl-4 pr-8 sm:pl-6 lg:pl-8">
-                  <div className="flex flex-col items-center justify-center gap-x-3 sm:flex-row">
-                    {item.logoUrl ? (
-                      <img
-                        src={item.logoUrl}
-                        alt={`${item.acronym} logo`}
-                        draggable={false}
-                        className="h-10 w-10 rounded-full"
-                      />
-                    ) : (
-                      <img
-                        src={PlaceholderLogo}
-                        alt="Placeholder logo"
-                        draggable={false}
-                        className="h-10 w-10 rounded-full blur-sm"
-                      />
-                    )}
-                    <div className="truncate text-base font-medium leading-6 text-white">
-                      {item.acronym}
+                #
+              </th>
+              <th
+                scope="col"
+                className="py-2 pl-4 pr-8 font-bold sm:pl-6 lg:pl-8"
+              >
+                Equipe
+              </th>
+              <th
+                scope="col"
+                className="py-2 pl-0 pr-4 font-bold text-center sm:pr-8 sm:text-center"
+              >
+                <span className="hidden sm:inline">Jogos</span>
+                <span className="sm:hidden">J</span>
+              </th>
+              <th
+                scope="col"
+                className="py-2 pl-0 pr-4 font-bold text-center sm:pr-8 sm:text-center"
+              >
+                <span className="hidden sm:inline">Vitórias</span>
+                <span className="sm:hidden">V</span>
+              </th>
+              <th
+                scope="col"
+                className="py-2 pl-0 pr-4 font-bold text-center sm:pr-8 sm:text-center"
+              >
+                <span className="hidden sm:inline">Derrotas</span>
+                <span className="sm:hidden">D</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-white/5">
+            {sortedItems &&
+              sortedItems.map((item) => (
+                <tr
+                  key={item.acronym}
+                  className="transition-all hover:bg-gray-700 cursor-pointer transform hover:scale-95"
+                >
+                  <td className="py-3 pl-0 pr-1 text-center sm:pr-8 sm:text-center">
+                    {item.position}
+                  </td>
+                  <td className="py-3 pl-4 pr-8 sm:pl-6 lg:pl-8">
+                    <div className="flex flex-col items-center justify-center gap-x-3 sm:flex-row">
+                      {item.logoUrl ? (
+                        <img
+                          src={item.logoUrl}
+                          alt={`${item.acronym} logo`}
+                          draggable={false}
+                          className="h-10 w-10 rounded-full"
+                        />
+                      ) : (
+                        <img
+                          src={PlaceholderLogo}
+                          alt="Placeholder logo"
+                          draggable={false}
+                          className="h-10 w-10 rounded-full blur-sm"
+                        />
+                      )}
+                      <div className="truncate text-base font-medium leading-6 text-white">
+                        {item.acronym}
+                      </div>
                     </div>
-                  </div>
-                </td>
-                <td className="py-3 pl-0 pr-4 text-center sm:pr-8 sm:text-center">
-                  {item.gamesPlayed}
-                </td>
-                <td className="py-3 pl-0 pr-4 text-center sm:pr-8 sm:text-center">
-                  {item.victories}
-                </td>
-                <td className="py-3 pl-0 pr-4 text-center sm:pr-8 sm:text-center">
-                  {item.loses}
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
+                  </td>
+                  <td className="py-3 pl-0 pr-4 text-center sm:pr-8 sm:text-center">
+                    {item.gamesPlayed}
+                  </td>
+                  <td className="py-3 pl-0 pr-4 text-center sm:pr-8 sm:text-center">
+                    {item.victories}
+                  </td>
+                  <td className="py-3 pl-0 pr-4 text-center sm:pr-8 sm:text-center">
+                    {item.loses}
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
