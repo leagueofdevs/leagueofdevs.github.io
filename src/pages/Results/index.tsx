935278467c1ee3f049cd5b6aa126f87e5e4dd041
Mateus Elias <meap@cin.ufpe.br>
2024-01-03 03:36:45 -0300

    feat(src/pages): add Breadcrumb component to Results and Teams pages

--- src/pages/Results/index.tsx
+++ src/pages/Results/index.tsx
@@ -1,18 +1,17 @@
+import Breadcrumb from "../../components/Breadcrumb";
 import Table from "../../components/Table";
 import Footer from "../../components/Footer";
-import { Link } from "react-router-dom";
 import { teams } from "../../utils/constants";
 
 function Results() {
   return (
     <div className="w-full h-full">
       <div className="mx-auto max-w-7xl px-6 text-center lg:px-8 pt-12">
-        <nav className="text-white flex justify-start w-full gap-x-1 mb-6">
-          <Link to="/" className="hover:underline">
-            Home
-          </Link>
-          / <span className="font-bold">Resultados</span>
-        </nav>
+        <Breadcrumb
+          previousPath="/"
+          previousPathName="Home"
+          currentPathName="Resultados"
+        />
         <div className="mx-auto max-w-2xl pb-12 ">
           <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
             Resultados
